1005298e0ba2990276da31c25426d56895cede04
chemi5t <umar.chemi5t@gmail.com>
2023-11-22 13:55:06 +0000

    M2/T3/Step2: Create method read_db_creds in class DatabaseConnector in database_utils.py.

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,24 @@
-Class DatabaseConnector
+import yaml # to read .yaml
+
+class DatabaseConnector:
+
+    @staticmethod
+    def read_db_creds(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                credentials = yaml.safe_load(file)
+                print(credentials)
+            return credentials
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Error: File {file_path} not found.")
+        except yaml.YAMLError as e:
+            raise yaml.YAMLError(f"Error while parsing YAML: {e}")
+
+# Usage:
+file_path = 'db_creds.yaml'  # Specify the correct file path
+try:
+    credentials = DatabaseConnector.read_db_creds(file_path)
+except FileNotFoundError as e:
+    print(e)
+except yaml.YAMLError as e:
+    print(e)

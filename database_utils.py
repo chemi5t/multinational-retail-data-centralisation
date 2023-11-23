6dc20c005263766178c08fb12e044e539daa6a3c
chemi5t <umar.chemi5t@gmail.com>
2023-11-23 13:03:08 +0000

    M2, T3, Step 4:create method list_db_tables to list all tables in db.

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
-import yaml # to read .yaml
+import yaml # to read .yaml. Help with read_db_creds
+import psycopg2 # a PostgreSQL adapter for Python
 
 class DatabaseConnector:
 
@@ -22,3 +23,15 @@ except FileNotFoundError as e:
     print(e)
 except yaml.YAMLError as e:
     print(e)
+
+
+# Now create a method init_db_engine which will read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine.
+
+    def init_db_engine(credentials):
+
+        with psycopg2.connect(host= credentials[RDS_HOST], user=credentials[RDS_USER], password=credentials[RDS_PASSWORD], dbname=credentials[RDS_DATABASE], port=credentials[RDS_PORT]) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT table_name FROM information_schema.tables
+            WHERE table_schema = 'public'""")
+                for table in cur.fetchall():
+                    print(table)
